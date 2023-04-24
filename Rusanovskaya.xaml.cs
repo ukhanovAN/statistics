b17f9f7d6210e9ad3da2ab166b30d8f6a2db5149
Rusanovskaya <bottom.rda@gmail.com>
2023-04-24 20:35:08 +0300

    Добавлен вывод графиков

--- Rusanovskaya.xaml.cs
+++ Rusanovskaya.xaml.cs
@@ -22,11 +22,27 @@ namespace gitt
     {
         public Rusanovskaya()
         {
-            
             InitializeComponent();
-            
+        }
+        private void buttton_One_Click(object sender, RoutedEventArgs e)
+        {
 
+            WpfPlot1.Plot.Clear();
+            DataCourse course = new DataCourse();
+            course.CoutOne();
+            double[] x = course.dateTimes.Select(x => x.ToOADate()).ToArray();
+            WpfPlot1.Plot.AddScatter(x, course.courseOne);
+            WpfPlot1.Plot.XAxis.DateTimeFormat(true);
+            WpfPlot1.Refresh();
         }
 
+        private void buttton_Two_Click(object sender, RoutedEventArgs e)
+        {
+
+            DataCourse course = new DataCourse();
+            course.CoutTwo();
+            WpfPlot1.Plot.Clear();
+            double[] x =
+
     }
-}
+    }
